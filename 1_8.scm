69b96852a502fb660f70e2aac2e42e04317d1bf2
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-07-30 17:19:06 +0530

    Finish 8th ex

---
+++ 1_8.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (cbrt-iter guess n)
+  (if (good-enough? guess n)
+      guess
+      (cbrt-iter (improve guess n) n)))
+
+(define (improve guess n)
+  (/ (+ (/ n (* guess guess)) (* 2 guess)) 3))
+
+(define (good-enough? guess n)
+  (<= (abs (- (improve guess n) guess)) (* 0.001 guess)))
+
+(cbrt-iter 1.0 (* 25 25 25))
