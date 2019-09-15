f1b0451eb5836fe6bb4f9abd95523693c93b3c9c
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-09-15 22:15:29 +0530

    Answer 1_36.scm

---
+++ 1_36.scm
@@ -0,0 +1,18 @@
+#lang racket
+(define tolerance 0.00001)
+(define (average x y) (/ (+ x y) 2))
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display next)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 2)
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 2)
+; Converges in 1/3rd the amount of iterations when using average damping
