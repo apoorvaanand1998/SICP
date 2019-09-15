ab627299e4bf0bb0f04fe4bea72be21ca37683a7
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-09-15 23:14:57 +0530

    Answer 1_37

---
+++ 1_37.scm
@@ -0,0 +1,29 @@
+#lang racket
+(define (cont-frac-rec n d k count)
+  (if (= count k)
+      (/ (n k) (d k))
+      (/ (n count) (+ (d count) (cont-frac-rec n d k (+ count 1))))))
+
+(define (try-rec k)
+  (if (< (abs (- (cont-frac-rec (lambda (i) 1.0)
+                 (lambda (i) 1.0)
+                 k
+                 1) 0.6180)) 0.0001)
+      (display k)
+      (try-rec (+ k 1))))
+
+(define (cont-frac-iter n d k count result)
+  (if (= 0 count)
+      result
+      (cont-frac-iter n d k (- count 1)
+                      (/ (n (- count 1)) (+ (d (- count 1)) result)))))
+
+(try-rec 1)
+(newline)
+(cont-frac-rec (lambda (i) 1.0)
+               (lambda (i) 1.0)
+               10 1)
+(cont-frac-iter (lambda (i) 1.0)
+                (lambda (i) 1.0)
+                9 9
+                (/ ((lambda (i) 1.0) 1.0) ((lambda (i) 1.0) 1.0)))
