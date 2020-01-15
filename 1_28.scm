4b3dbf62fb9e05d87f33d7f238b741306c428f09
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-01-15 20:11:53 +0530

    Refactor 1_28

--- 1_28.scm
+++ 1_28.scm
@@ -1,24 +1,44 @@
 #lang sicp
 
-(define (ntsr n m)
-  (if (and (not (or (= n 1)
-                    (= n (- m 1))))
-           (= (remainder (* n n) m) 1))
-      0
-      (remainder (* n n) m)))
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square n)
+  (* n n))
 
 (define (expmod base exp m)
+  (define (check-ntsr? x)
+    (if (and (not (= x 1))
+             (not (= x (- m 1)))
+             (= (remainder (square x) m) 1)) ;"whose square is equal to 1 modulo n"
+        ; and initially, (= (square x) 1) was tried. Book should say CONGRUENT TO 1 modulo n instead
+        0
+        (square x)))
   (cond ((= exp 0) 1)
         ((even? exp)
-         (ntsr (expmod base (/ exp 2) m) m))
-        (else
-         (remainder (* base (expmod base (- exp 1) m))
-                    m))))
+         (remainder (check-ntsr? (expmod base (/ exp 2) m))
+                    m))
+        (else (remainder (* base
+                            (expmod base (- exp 1) m))
+                         m))))
 
-(define (miller-rabin-test n)
+(define (miller-rabin n)
   (define (try-it a)
-    (= (expmod a (- n 1) n) 1))
+    (not (= (expmod a (- n 1) n) 0)))
   (try-it (+ 1 (random (- n 1)))))
 
-(miller-rabin-test 561) ; Should return false
-(miller-rabin-test 17) 
+(define (miller-rabin-test n)
+  (define (run-x-times x)
+    (cond ((= x 0) true)
+          ((miller-rabin n)
+           (run-x-times (- x 1)))
+          (else false)))
+  (run-x-times (+ 2 (quotient n 2))))
+
+(miller-rabin-test 3) ;should return t
+(miller-rabin-test 5)
+(miller-rabin-test 15) ;should return f for all below
+(miller-rabin-test 27)
+(miller-rabin-test 561)
+(miller-rabin-test 1729)
+(miller-rabin-test 6601)
