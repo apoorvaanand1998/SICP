70c65eeb988e8558305fd1b4e1e07f7bae7295c4
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-01-19 22:12:17 +0530

    Refactor 1_35 and also answer first part of the question

--- 1_35.scm
+++ 1_35.scm
@@ -1,14 +1,14 @@
-#lang racket
-(define tolerance 0.00001)
-(define (fixed-point f first-guess)
-  (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (close-enough? guess next)
-          next
-          (try next))))
-  (try first-guess))
+#lang sicp
 
-(/ (+ 1 2.236) 2) ; square root of 5 is 2.236
-(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+; We know that the golden ratio is the root of the quation x^2 = x+1
+; Dividing both sides by x, we get x = 1 + (1/x). Proved!
+
+(define (fixed-point f initial-guess)
+  (define (close-enough? guess)
+    (< (abs (- guess (f guess))) 0.00001))
+  (let ((guess (f initial-guess)))
+    (if (close-enough? guess)
+        (f guess)
+        (fixed-point f guess))))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0) ; it works! = (1+sqrt(5))/2
