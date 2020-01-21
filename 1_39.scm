383561682a50ae4e37eabbe799f0143d7306025a
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-01-21 22:24:50 +0530

    Refactor 1_39

--- 1_39.scm
+++ 1_39.scm
@@ -1,18 +1,17 @@
-#lang racket
+#lang sicp
 
-(define (cont-frac-iter n d k count result)
-  (if (= 1 count)
-      result
-      (cont-frac-iter n d k (- count 1)
-                      (/ (n (- count 1)) (+ (d (- count 1)) result)))))
+(define (cont-frac n d k)
+  (define (cf-helper mid-frac k)
+    (if (= k 0)
+        mid-frac
+        (cf-helper (/ (n k) (+ (d k) mid-frac)) (- k 1))))
+  (cf-helper 0 k))
 
-(define (tan-cf x k) 
-  (cont-frac-iter (lambda (i) 
-               (if (= i 1) x (- (* x x)))) 
-             (lambda (i) 
-               (- (* i 2) 1)) 
-             k k (/ ((lambda (i) 
-               (if (= i 1) x (- (* x x)))) k) ((lambda (i) 
-               (- (* i 2) 1)) k))))
+(define (tan-cf x k)
+  (cont-frac (lambda (i) (if (= i 1)
+                             x
+                             (- (* x x))))
+             (lambda (i) (- (* 2 i) 1))
+             k))
 
-(tan-cf 3.14 5) ; Should be around 0. Is around 0 :)
+(tan-cf 3.14 100)
