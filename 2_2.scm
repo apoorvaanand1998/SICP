8d74d707ab727a1c189b10a45781086b8fbb24d2
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-04-23 16:09:27 +0530

    Solve 2_2

---
+++ 2_2.scm
@@ -0,0 +1,43 @@
+#lang sicp
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment seg)
+  (car seg))
+
+(define (end-segment seg)
+  (cdr seg))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (midpoint-segment seg)
+  (make-point (average (x-point (start-segment seg))
+                       (x-point (end-segment seg)))
+              (average (y-point (start-segment seg))
+                       (y-point (end-segment seg)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define seg (make-segment (make-point 5 6) 
+                           (make-point 7 12))) 
+  
+(print-point (midpoint-segment seg)) 
+
+
