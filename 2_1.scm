1171cf2df086f752d6e8a1ec2036116f1b9daa65
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-05-16 17:05:08 +0530

    Add more tests. Fix bug found

--- 2_1.scm
+++ 2_1.scm
@@ -2,14 +2,14 @@
 
 (define (gcd a b)
   (if (= b 0)
-      a
+      (abs a)
       (gcd b (remainder a b))))
 
 (define (make-rat n d)
   (let ((g (gcd n d)))
     (if (< (* n d) 0)
-        (cons (/ n g) (abs (/ d g)))
-        (cons (/ n g) (/ d g)))))
+        (cons (- (abs (/ n g))) (abs (/ d g)))
+        (cons (abs (/ n g)) (abs (/ d g))))))
 
 (define (numer x)
   (car x))
@@ -26,6 +26,8 @@
 (print-rat (make-rat 4 8))
 (print-rat (make-rat -2 -4))
 (print-rat (make-rat -6 9))
+(print-rat (make-rat -9 6))
+(print-rat (make-rat 9 -6))
 (print-rat (make-rat 6 -9))
 (print-rat (make-rat 55 0)) ; should be handled (not in this exercise though)
 
