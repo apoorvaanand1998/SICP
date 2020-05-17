f345c48f5d8be2af033b6227a41537b0d4293d83
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-05-17 11:30:56 +0530

    Solve 2_8

---
+++ 2_8.scm
@@ -0,0 +1,29 @@
+#lang sicp
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound interval)
+  (max (car interval) (cdr interval)))
+
+(define (lower-bound interval)
+  (min (car interval) (cdr interval)))
+
+(define (add-interval a b)
+  (make-interval (+ (lower-bound a) (lower-bound b))
+                 (+ (upper-bound a) (upper-bound b))))
+
+(define (mul-interval a b)
+  (let ((p1 (* (lower-bound a) (lower-bound b)))
+        (p2 (* (lower-bound a) (upper-bound b)))
+        (p3 (* (upper-bound a) (lower-bound b)))
+        (p4 (* (upper-bound a) (upper-bound b))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval a b)
+  (mul-interval a
+                (make-interval (/ 1 (upper-bound b)) (/ 1 (lower-bound b)))))
+
+(define (sub-interval a b)
+  (add-interval a
+                (make-interval (- (upper-bound b)) (- (lower-bound b)))))
+
